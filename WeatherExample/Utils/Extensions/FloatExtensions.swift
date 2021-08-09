cabe3dc1ae810fd7af4360aeb4fbd0b7b5fe3f78
Valeriya Chubakova <Valeri160500@gmail.com>
2021-08-09 13:48:44 +0300

    Change FloatExtensions

---
+++ WeatherExample/Utils/Extensions/FloatExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  FloatExtensions.swift
+//  WeatherExample
+//
+//  Created by User on 9.08.21.
+//
+
+import Foundation
+
+extension Float {
+    func toCelcium() -> Float {
+        return self - 273.15
+    }
+    
+    func format(f: String) -> String {
+        // .3 = "0.000"
+        // .1 = "0.0"
+        return String(format: "%\(f)f", self)
+    }
+    
+}
